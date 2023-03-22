6dc0be18e0e1cb3ddb11a440e210f919c2ed97ec
Vahan Nadjarian <vahan@nadjarian.com>
2023-03-22 14:37:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+const identity = newHeader.setAttribute("id", "victory");
+
+const message = "Vahan is the champion"
+
+newHeader.innerHTML = message
+
